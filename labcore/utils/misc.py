d297f78bfc13011749e2a5466aaabaa6f0ac07dc
Jeremy Lin <jl190@illinois.edu>
2023-09-07 14:43:16 -0500

    added measurement directory from labcore old and labcore.util functions to make record and sweep not throw import errors

--- labcore/utils/misc.py
+++ labcore/utils/misc.py
@@ -4,7 +4,8 @@ Various utility functions.
 """
 
 from enum import Enum
-from typing import List, Tuple, TypeVar, Optional, Sequence, Any
+from typing import List, Tuple, TypeVar, Optional, Sequence, Any, Callable, Union
+import inspect
 
 
 def reorder_indices(lst: Sequence[str], target: Sequence[str]) -> Tuple[int, ...]:
@@ -112,3 +113,83 @@ class LabeledOptions(AutoEnum):
             if k.label.lower() == label.lower():
                 return k
         return None
+
+
+# FIXME: 'None' should never override a default!
+def map_input_to_signature(func: Union[Callable, inspect.Signature],
+                           *args: Any, **kwargs: Any):
+    """Try to re-organize the positional arguments `args` and key word
+    arguments `kwargs` such that `func` can be called with them.
+
+    if `func` expects arguments that cannot be given, they will be given
+    as ``None``.
+    Surplus arguments are ignored if `func` does not accept variable positional
+    and/or keyword arguments.
+    Example::
+
+        >>> def myfunc(x, y, z=1):
+        ...     print(f"x={x}, y={y}, z={z}")
+        ...
+        ... args, kwargs = map_input_to_signature(myfunc, z=1, x=1, unused=4)
+        ... myfunc(*args, **kwargs)
+        x=1, y=None, z=1
+
+    It is important to note that the position of positional arguments is not
+    preserved, because input key words that match expected positional arguments
+    are inserted as positional arguments at the right position. The order,
+    however, is preserved. Example::
+
+        >>> def myfunc(x, y, z):
+        ...     print(f"x={x}, y={y}, z={z}")
+        ...
+        ... args, kwargs = map_input_to_signature(myfunc, 1, 2, x=5)
+        ... myfunc(*args, **kwargs)
+        x=5, y=1, z=2
+    """
+    args = list(args)
+    func_args = []
+    func_kwargs = {}
+
+    if isinstance(func, inspect.Signature):
+        sig = func
+    else:
+        sig = inspect.signature(func)
+
+    # Logic:
+    # for each param the function expects, we need to check if have
+    # received a fitting one
+    for idx, p in enumerate(sig.parameters):
+        p_ = sig.parameters[p]
+
+        # we treat anything that can be given positionally as positional.
+        # first prio to keyword-given values, second to positionally given,
+        # finally default if given in signature.
+        if p_.kind in [inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                       inspect.Parameter.POSITIONAL_ONLY]:
+            if p in kwargs:
+                func_args.insert(idx, kwargs.pop(p))
+            else:
+                if len(args) > 0:
+                    func_args.insert(idx, args.pop(0))
+                elif p_.default is inspect.Parameter.empty:
+                    func_args.insert(idx, None)
+                else:
+                    func_args.insert(idx, p_.default)
+
+        elif p_.kind is inspect.Parameter.KEYWORD_ONLY:
+            if p in kwargs:
+                func_kwargs[p] = kwargs.pop(p)
+
+        elif p_.kind is inspect.Parameter.VAR_POSITIONAL:
+            for a in args:
+                func_args.append(a)
+
+        elif p_.kind is inspect.Parameter.VAR_KEYWORD:
+            func_kwargs.update(kwargs)
+
+    return func_args, func_kwargs
+
+
+def indent_text(text: str, level: int = 0) -> str:
+    """Indent each line of ``text`` by ``level`` spaces."""
+    return "\n".join([" " * level + line for line in text.split('\n')])
