c07b1546ff6fd416de16ff876f4eeabd1d623e83
Oliver Wolff <owolff2@illinois.edu>
2024-07-24 16:41:35 -0500

    adding dataclass import

--- labcore/instruments/opx/mixer.py
+++ labcore/instruments/opx/mixer.py
@@ -4,7 +4,6 @@ Required packages/hardware:
 - QM OPX incl python software
 - SignalHound USB SA124B + driver (comes with qcodes)
 """
-from dataclasses import dataclass
 from functools import partial
 from typing import List, Tuple, Optional, Any, Dict, Callable
 from time import sleep
